076606448db0cff4840c732846fc5c1eb6d04bfa
jathintk <jathintk@gmail.com>
2025-11-17 20:23:18 +0530

    testsf

--- rest-fights/src/main/java/io/quarkus/sample/superheroes/fight/service/FightService.java
+++ rest-fights/src/main/java/io/quarkus/sample/superheroes/fight/service/FightService.java
@@ -42,7 +42,7 @@ import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.MutinyEmitter;
 
 /**
- * TESTING
+ * TESTING  latest
  * Business logic for the Fight service
  */
 @ApplicationScoped
